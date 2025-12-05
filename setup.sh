3f47a98c7d2363fcb36f11316402615a14d4c9de
Drs2001 <dylanspence705607@gmail.com>
2025-12-04 21:04:14 -0500

    added check to setup for nvidia gpu

--- setup.sh
+++ setup.sh
@@ -2,7 +2,16 @@
 
 scripts/yay_setup.sh
 scripts/install_packages.sh
-scripts/nvidia.sh
 scripts/firewall_setup.sh
 scripts/dots_setup.sh
 scripts/regreet_setup.sh
+
+# Check for gpu and install appropriate drivers
+# (Need to update in the future to install intel / AMD specific drivers
+#   although those should just work out of the box)
+if lspci | grep -i nvidia > /dev/null 2>&1; then
+    echo "NVIDIA GPU detected — running nvidia.sh"
+    scripts/nvidia.sh
+else
+    echo "No NVIDIA GPU detected — skipping NVIDIA setup"
+fi
