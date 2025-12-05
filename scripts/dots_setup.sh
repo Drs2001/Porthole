c6e4a4730ee15fbec0794c3e818a1782507681fb
Drs2001 <dylanspence705607@gmail.com>
2025-12-04 21:25:15 -0500

    moved package.txt files and changed dots path

--- scripts/dots_setup.sh
+++ scripts/dots_setup.sh
@@ -4,13 +4,13 @@
 git clone https://github.com/Drs2001/PortholeDE.git ~/.config/quickshell/porthole
 
 # Copy rofi dots
-cp -r ../dots/rofi ~/.config/
+cp -r dots/rofi ~/.config/
 
 # Copy Hyprland dots
-cp -r ../dots/hypr ~/.config/
+cp -r dots/hypr ~/.config/
 
 # Copy qt6 dots
-cp -r ../dots/qt6ct ~/.config/
+cp -r dots/qt6ct ~/.config/
 
 # Download and install Tela icon theme
 git clone https://github.com/vinceliuice/Tela-icon-theme.git
