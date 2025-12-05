49e2fd52b80e214fe4d6258ca4352b579515cad8
Drs2001 <dylanspence705607@gmail.com>
2025-12-04 20:51:05 -0500

    Updated dots to install icon pack and cursor theme

--- scripts/dots_setup.sh
+++ scripts/dots_setup.sh
@@ -11,3 +11,15 @@ cp -r ../dots/hypr ~/.config/
 
 # Copy qt6 dots
 cp -r ../dots/qt6ct ~/.config/
+
+# Download and install Tela icon theme
+git clone https://github.com/vinceliuice/Tela-icon-theme.git
+cd Tela-icon-theme
+install.sh
+cd ..
+
+# Download and install We10XOS cursors
+git clone https://github.com/yeyushengfan258/We10XOS-cursors.git
+cd We10XOS-cursors
+install.sh
+cd ..
