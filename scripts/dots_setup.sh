d55901167413508c9c387871f85d38c06c79c12e
Drs2001 <dylanspence705607@gmail.com>
2026-01-17 01:08:13 -0500

    removed rofi app launcher

--- scripts/dots_setup.sh
+++ scripts/dots_setup.sh
@@ -3,9 +3,6 @@
 # Download Porthole Desktop Environment dots
 git clone https://github.com/Drs2001/PortholeDE.git ~/.config/quickshell/porthole
 
-# Copy rofi dots
-cp -r dots/rofi ~/.config/
-
 # Copy Hyprland dots
 cp -r dots/hypr ~/.config/
 
