c30b4282d5761fa8f923c8165ac0d779b93b4ba5
Drs2001 <dylanspence705607@gmail.com>
2026-05-22 17:45:59 -0400

    Add live user to sudoers file

--- porthole-iso/profiledef.sh
+++ porthole-iso/profiledef.sh
@@ -24,4 +24,6 @@ file_permissions=(
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/etc/shadow"]="0:0:0400"
   ["/etc/gshadow"]="0:0:0400"
+  ["/etc/sudoers"]="0:0:0440"
+  ["/etc/sudoers.d/liveuser"]="0:0:0440"
 )
